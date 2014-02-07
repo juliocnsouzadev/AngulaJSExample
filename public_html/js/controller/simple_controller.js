fc8c7efbab5dda6df6d23f48b893aeee85326cb0
juliocnsouzadev <juliocnsouzadev@gmail.com>
2014-02-06 22:12:24 -0200

    #simpleController update

--- public_html/js/controller/simple_controller.js
+++ public_html/js/controller/simple_controller.js
@@ -2,3 +2,9 @@ function simpleController($scope)
 {
     $scope.user = {name: '', age: ''};
 }
+
+function simpleMult($scope)
+{
+    $scope.values = {number0ne: 1, numberTwo: 1};
+    $scope.result = $scope.values.number0ne * $scope.values.numberTwo;
+}
